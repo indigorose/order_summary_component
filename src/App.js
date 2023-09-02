b8ca3ea478611ddb108dbe91914bc343b288cd6c
indigorose <indigorose049@gmail.com>
2023-09-02 17:23:03 +0100

    Add basic css

--- src/App.js
+++ src/App.js
@@ -6,28 +6,26 @@ function App() {
 	return (
 		<div className="App">
 			<div className="container">
-				<div className="card">
-					<img
-						src={heroImage}
-						alt="Girl with headphones"
-						className="icon"
-					/>
-					<div className="card-summary">
-						<h1>Order Summary</h1>
-						<p className="order-text">
-							You can now listen to millions of songs, audiobooks,
-							and podcasts on any device anywhere you like.{' '}
-						</p>
-					</div>
-					<div className="card-price">
-						<img src={icon} alt="musical note" />
-						<h4>Annual Plan</h4>
-						<span>$59.99/year</span>
-						<button>Change</button>
-					</div>
-					<button>Proceed to Payment</button>
-					<button>Cancel Order</button>
+				<img
+					src={heroImage}
+					alt="Girl with headphones"
+					className="icon-image"
+				/>
+				<div className="card-summary">
+					<h1>Order Summary</h1>
+					<p className="order-text">
+						You can now listen to millions of songs, audiobooks, and
+						podcasts on any device anywhere you like.
+					</p>
 				</div>
+				<div className="card-price">
+					<img src={icon} alt="musical note" className="music-note" />
+					<h4>Annual Plan</h4>
+					<span>$59.99/year</span>
+					<button>Change</button>
+				</div>
+				<button className="payment-btn">Proceed to Payment</button>
+				<button className="cancel-btn">Cancel Order</button>
 			</div>
 		</div>
 	);
